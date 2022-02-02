2dede52a0147ea28b7c9744a473eea201c524eb4
RhysH0w <93980599+RhysH0w@users.noreply.github.com>
2022-02-02 14:40:47 +0000

    Create static.py

---
+++ static.py
@@ -0,0 +1,16 @@
+# Import libraries
+import os
+import pygame
+
+pygame.init()
+
+ScreenSizeInfo = pygame.display.Info()  # Get the info for the size of the monitor
+ScreenSize = (ScreenSizeInfo.current_w, ScreenSizeInfo.current_h)  # Define the size of the screen
+
+# Colours
+black = (0, 0, 0)
+red = (255, 0, 0)
+blue = (0,255,0)
+lightred = (100,0,0)
+word = os.environ['character']
+font = pygame.font.SysFont('courier', 35, True, False)
