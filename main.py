8e50db80ba2a7458f66b379d48d1905a3b837e31
RhysH0w <93980599+RhysH0w@users.noreply.github.com>
2022-02-02 14:40:28 +0000

    Create main.py

---
+++ main.py
@@ -0,0 +1,77 @@
+import os
+# Import the libraries
+import pygame
+from static import *
+from Button import *
+# Initialise the engines
+pygame.init()
+print(word)
+clock = pygame.time.Clock()
+
+def Home():  # This is the home page of my pong game
+  click = False
+  while True:
+
+        mx, my = pygame.mouse.get_pos()
+        mouse = (mx, my)
+        gameDisplay.fill(black)
+        PongButton = pygame.Rect(20, 150, 250, 60)
+        SpaceButton = pygame.Rect(20, 250, 250, 60)
+
+        PongButtonText = Button("Pong", 20, 150, font)
+        SpaceButtonText = Button("Space Invaders", 20, 250, font)
+
+        PongButtonText.write(gameDisplay, red)
+        SpaceButtonText.write(gameDisplay, red)
+
+        if PongButton.collidepoint(mouse):
+            PongButtonText.write(gameDisplay, lightred)
+            if click:
+              Pong()
+
+        if SpaceButton.collidepoint(mouse):
+            SpaceButtonText.write(gameDisplay, lightred)
+            if click:
+              Space()
+      
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if event.button == 1:
+                    click = True
+
+        pygame.display.update()
+        clock.tick(60)
+
+
+def Pong():
+    click = False
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE:
+                    pygame.quit()
+
+        gameDisplay.fill(black)
+        pygame.draw.rect(gameDisplay, blue, [100,100,100,100])
+        pygame.display.update()
+
+
+gameDisplay = pygame.display.set_mode(ScreenSize)
+pygame.display.set_caption("Pong")
+
+def Space():
+    click = False
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+
+            i
+Home()
